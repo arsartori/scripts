798afe9fe27a898a168128d5cfa9e04cb4bc990c
André Sartori <arsartori@outlook.com>
2023-01-12 17:48:39 -0300

    Update nginx-install.sh
    Add option "-y" in apt install.

--- nginx-install.sh
+++ nginx-install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-sudo apt install curl gnupg2 ca-certificates lsb-release ubuntu-keyring
+sudo apt install -y curl gnupg2 ca-certificates lsb-release ubuntu-keyring
 curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor \
     | sudo tee /usr/share/keyrings/nginx-archive-keyring.gpg > /dev/null
 gpg --dry-run --quiet --no-keyring --import --import-options import-show /usr/share/keyrings/nginx-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] \
 http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" \
     | sudo tee /etc/apt/sources.list.d/nginx.list
-sudo apt update && sudo apt install nginx
+sudo apt update && sudo apt install -y nginx
