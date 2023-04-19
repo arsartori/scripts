950d39b13a5defd9063fc1a2afba69bb1f871903
André Sartori <49533777+arsartori@users.noreply.github.com>
2023-04-19 10:48:52 -0300

    Update wsl-install.cmd

--- wsl-install.cmd
+++ wsl-install.cmd
@@ -1,19 +1,27 @@
-# Habilitar o Subsistema do Windows para Linux
+# Enable the Windows Subsystem for Linux
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 
-# Habilitar o recurso de Máquina Virtual
+# Enable Virtual Machine feature
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
 
-# Baixar o pacote de atualização do kernel do Linux
+# Download the Linux kernel update package
 https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi
 
-# Definir o WSL 2 como a sua versão padrão
+# Set the default version
 wsl --set-default-version 2
 
+# Upgrade version from WSL 1 to WSL 2 
+wsl --set-version <distro name> 2
+
 # Download distribution from store
-https://www.microsoft.com/store/apps/9PN20MSR04DW
+Ubuntu - https://www.microsoft.com/store/apps/9PN20MSR04DW
 
 # Download distribution manualmente
-Opção 1 - https://aka.ms/wslubuntu2204
-Opção 2 - Invoke-WebRequest -Uri https://aka.ms/wslubuntu2004 -OutFile Ubuntu.appx -UseBasicParsing
-Opção 3 - curl.exe -L -o ubuntu-2004.appx https://aka.ms/wslubuntu2004
+Option 1 - https://aka.ms/wslubuntu2204
+Option 2 - Invoke-WebRequest -Uri https://aka.ms/wslubuntu2204 -OutFile Ubuntu.appx -UseBasicParsing
+option 3 - curl.exe -L -o ubuntu-2204.appx https://aka.ms/wslubuntu2204
+
+# Downloading distributions
+Debian - https://aka.ms/wsl-debian-gnulinux
+Suse - https://aka.ms/wsl-SUSELinuxEnterpriseServer15SP3
+Oracle - https://aka.ms/wsl-oraclelinux-8-5
